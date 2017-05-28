071df1bbb8a3f3acaa9e06dcfa700d0bfcb008b0
Dirk-Tooth <trevormckay.davis@gmail.com>
2017-05-28 13:35:32 -0500

    meow...

--- src/components/app.js
+++ src/components/app.js
@@ -8,69 +8,69 @@ import { mostOf, calcPlayerScore } from '../util/scoreCalcs';
 const colors = ['green', 'blue', 'red', 'white', 'orange', 'brown'];
 
 class Default extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			players: [],
-			winPoint: 20,
-			masters: {
-				roads: '',
-				harbors: '',
-				knights: '',
-				fish: '',
-				trade: '',
-				politics: '',
-				science: '',
-			},
-		};
-		for (let i = 0; i < 3; i += 1) this.state.players.push(protoPlayer(colors[i]));
-	}
-	changeTopLevelState(key, value) {
-		this.setState({ [key]: value });
-	}
-	handleShiftPlayers(action) {
-		action > 0 ?
+  constructor() {
+    super();
+    this.state = {
+      players: [],
+      winPoint: 20,
+      masters: {
+        roads: '',
+        harbors: '',
+        knights: '',
+        fish: '',
+        trade: '',
+        politics: '',
+        science: '',
+      },
+    };
+    for (let i = 0; i < 3; i += 1) this.state.players.push(protoPlayer(colors[i]));
+  }
+  changeTopLevelState(key, value) {
+    this.setState({ [key]: value });
+  }
+  handleShiftPlayers(action) {
+    action > 0 ?
 		this.setState({ players: [...this.state.players, protoPlayer] }) :
 		this.setState({ players: this.state.players.slice(0, this.state.players.length - 1) });
-	}
-	changeScoreButton(color, scoreItem, delta) {
-		const newPlayers = this.state.players.map(item => item.color === color ?
+  }
+  changeScoreButton(color, scoreItem, delta) {
+    const newPlayers = this.state.players.map(item => item.color === color ?
 			Object.assign({}, item, { [scoreItem]: item[scoreItem] + delta }) :
 			item);
-		this.setState({ players: newPlayers,
-			masters: this.state.masters.hasOwnProperty(scoreItem) ?
+    this.setState({ players: newPlayers,
+      masters: this.state.masters.hasOwnProperty(scoreItem) ?
 				Object.assign({}, this.state.masters, {
-					[scoreItem]: mostOf(newPlayers, scoreItem) === '' ?
+  [scoreItem]: mostOf(newPlayers, scoreItem) === '' ?
 						this.state.masters[scoreItem] :
 						mostOf(newPlayers, scoreItem),
-				}) :
+}) :
 				this.state.masters,
-		});
-	}
+    });
+  }
 
-	render() {
-		return (
-  <div>
-    <HeaderBar
-      players={this.state.players}
-      winPoint={this.state.winPoint}
-      onClick={(key, value) => this.changeTopLevelState(key, value)}
-      onChange={action => this.handleShiftPlayers(action)}
+  render() {
+    return (
+      <div>
+        <HeaderBar
+          players={this.state.players}
+          winPoint={this.state.winPoint}
+          onClick={(key, value) => this.changeTopLevelState(key, value)}
+          onChange={action => this.handleShiftPlayers(action)}
 		/>
-    <div className="player-container">
-      {this.state.players.map((item, idx) => <Player
-        className="player"
-        key={idx}
-        score={calcPlayerScore(item, this.state.masters, this.state.players)}
-        winPoint={this.state.winPoint}
-        playerState={item}
-        incrementerButton={(color, scoreItem, delta) => this.changeScoreButton(color, scoreItem, delta)}
+        <div className="player-container">
+          {this.state.players.map((item, idx) => <Player
+            className="player"
+            key={idx}
+            score={calcPlayerScore(item, this.state.masters, this.state.players)}
+            winPoint={this.state.winPoint}
+            playerState={item}
+            incrementerButton={(color, scoreItem, delta) => this.changeScoreButton(color, scoreItem, delta)}
 							/>,
 			)}
-    </div>
-  </div>
-		);
-	}
+        </div>
+      </div>
+    );
+  }
 }
 
 module.exports = Default;
