18d716a241e622ec0bd5bea215f74e5e4520ecc2
Dirk-Tooth <trevormckay.davis@gmail.com>
2017-06-19 08:05:13 -0500

    Fixed ghost points issue

--- src/components/header.js
+++ src/components/header.js
@@ -1,39 +1,39 @@
 import React from 'react';
-import {Button, Navbar, Header, Brand, Toggle, Collapse, Nav, NavItem, MenuItem, NavDropdown} from 'react-bootstrap';
+import { Button, Navbar, Header, Brand, Toggle, Collapse, Nav, NavItem, MenuItem, NavDropdown } from 'react-bootstrap';
 
 class HeaderBar extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-	render() {
-		return (
-		  <Navbar inverse collapseOnSelect className="header-bar">
-			<div className="title">KATANNN!!!</div>
-			<div className="playerCount">
-				<Button
-					className="minus-btn"
-					onClick={() => this.props.onChange(0)}
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <Navbar inverse collapseOnSelect className="header-bar">
+        <div className="title">KATANNN!!!</div>
+        <div className="playerCount">
+          <Button
+            className="minus-btn"
+            onClick={() => this.props.onChange(0)}
 				>-</Button>
-				<div>{`${this.props.players.length} players`}</div>
-				<Button
-					className="plus-btn"
-					onClick={() => this.props.onChange(1)}
+          <div>{`${this.props.players.length} players`}</div>
+          <Button
+            className="plus-btn"
+            onClick={() => this.props.onChange(1)}
 				>+</Button>
-			</div>
-			<div className="winPoint">
-				<Button
-					className="minus-btn"
-					onClick={() => this.props.onClick('winPoint', this.props.winPoint - 1)}
+        </div>
+        <div className="winPoint">
+          <Button
+            className="minus-btn"
+            onClick={() => this.props.onClick('winPoint', this.props.winPoint - 1)}
 				>-</Button>
-				<div>{`${this.props.winPoint} points`}</div>
-				<Button
-					className="plus-btn"
-					onClick={() => this.props.onClick('winPoint', this.props.winPoint + 1)}
+          <div>{`${this.props.winPoint} points`}</div>
+          <Button
+            className="plus-btn"
+            onClick={() => this.props.onClick('winPoint', this.props.winPoint + 1)}
 				>+</Button>
-			</div>
-		  </Navbar>
-		);
-	}
+        </div>
+      </Navbar>
+    );
+  }
 }
 
 module.exports = HeaderBar;
